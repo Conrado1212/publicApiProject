1c7b2255c87b810dfa3d80524137fa8e247f862f
Conrado1212 <conrado1212@gmail.com>
2026-02-04 17:55:40 +0100

    overview

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ import multer from "multer";
 const app = express();
 const port  = 3000;
 
+const API_URL ='';
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use( express.static("style"));
+
+const storage = multer.diskStorage({
+    destination :(req, file, cb)=>{
+      cb(null, "style/images");
+    },
+    filename: (req, file, cb)=>{
+      cb(null, file.originalname);
+    }
+  });
+
+const upload = multer({storage})
+
+app.get('/', (req,res)=>{
+    res.render("index.ejs");
+});
 
 
 app.listen(port,()=>{
