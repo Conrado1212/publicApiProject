ff58135be1289a458e2c6ff8dd67ea6b4eb0f420
Conrado1212 <conrado1212@gmail.com>
2026-02-19 17:54:54 +0100

    overview

--- style/app.js
+++ style/app.js
@@ -3,3 +3,14 @@ console.log(main);
 main.addEventListener('click',()=>{
     window.location.href = "http://localhost:3000"
 })
+
+const searchInput = document.querySelector('#search-input');
+
+searchInput.addEventListener('focus', () => {
+    searchInput.dataset.placeholder = searchInput.placeholder; 
+    searchInput.placeholder = '';
+});
+
+searchInput.addEventListener('blur', () => {
+    searchInput.placeholder = searchInput.dataset.placeholder;
+});
