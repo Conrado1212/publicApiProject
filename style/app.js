75fb44e431206c83137725727ebc6cdd13e35fe4
Conrado1212 <conrado1212@gmail.com>
2026-04-12 16:02:21 +0200

    overview

--- style/app.js
+++ style/app.js
@@ -1,4 +1,5 @@
 const main = document.querySelector('.logo');
+const showMenu = document.querySelector('.discover');
 const searchInput = document.querySelector('#search-input');
 //console.log(main);
 main.addEventListener('click',()=>{
@@ -15,3 +16,22 @@ searchInput.addEventListener('focus', () => {
 searchInput.addEventListener('blur', () => {
     searchInput.placeholder = searchInput.dataset.placeholder;
 });
+
+showMenu.addEventListener('click',()=>{
+    if(document.querySelector('.discover > span').textContent === "Show All"){
+        showMenu.querySelector('span').style.transform = "rotate(45deg)";
+        showMenu.querySelectorAll('span').forEach(span=>{
+            span.style.bottom = "60%";
+    });
+    showMenu.querySelector('span:nth-child(2)').style.transform = "rotate(135deg)";
+    document.querySelector('.discover > span').textContent ="Hide All";
+    }else{
+        showMenu.querySelector('span').style.transform = "rotate(-45deg)";
+        showMenu.querySelectorAll('span').forEach(span=>{
+            span.style.bottom = "30%";
+    });
+        showMenu.querySelector('span:nth-child(2)').style.transform = "rotate(-135deg)";
+    document.querySelector('.discover > span').textContent ="Show All";
+    }
+    
+})
