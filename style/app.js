636d3e6e1a030367d4fe3247d4bd124df4e87f01
Conrado1212 <conrado1212@gmail.com>
2026-04-20 19:49:05 +0200

    overview

--- style/app.js
+++ style/app.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('.logo');
 const showMenu = document.querySelector('.discover');
+const showMenuu = document.querySelectorAll('.discover');
 const searchInput = document.querySelector('#search-input');
 const modeSw = document.querySelector(".toggle-sw");
 //console.log(main);
@@ -21,21 +22,39 @@ searchInput.addEventListener('blur', () => {
     searchInput.placeholder = searchInput.dataset.placeholder;
 });
 
-showMenu.addEventListener('click',()=>{
-    if(document.querySelector('.discover > span').textContent === "Show All"){
-        showMenu.querySelector('span').style.transform = "rotate(45deg)";
-        showMenu.querySelectorAll('span').forEach(span=>{
-            span.style.bottom = "60%";
+
+showMenuu.forEach(menu =>{
+    menu.addEventListener('click',()=>{
+        const text = menu.querySelector('span');
+        const spans = menu.querySelectorAll('span');
+        const second = menu.querySelector('span:nth-child(2)');
+
+        const isShow = text.textContent ==="Show All";
+
+        text.style.transform = isShow ? "rotate(45deg)": "rotate(-45deg)";
+        second.style.transform = isShow ? "rotate(135deg)": "rotate(-135deg)";
+        spans.forEach(span => {
+            span.style.bottom = isShow ? "60%" : "30%";
+        });
+        text.textContent = isShow ? "Hide All" : "Show All";
     });
-    showMenu.querySelector('span:nth-child(2)').style.transform = "rotate(135deg)";
-    document.querySelector('.discover > span').textContent ="Hide All";
-    }else{
-        showMenu.querySelector('span').style.transform = "rotate(-45deg)";
-        showMenu.querySelectorAll('span').forEach(span=>{
-            span.style.bottom = "30%";
-    });
-        showMenu.querySelector('span:nth-child(2)').style.transform = "rotate(-135deg)";
-    document.querySelector('.discover > span').textContent ="Show All";
-    }
+});
+
+// showMenu.addEventListener('click',()=>{
+//     if(document.querySelector('.discover > span').textContent === "Show All"){
+//         showMenu.querySelector('span').style.transform = "rotate(45deg)";
+//         showMenu.querySelectorAll('span').forEach(span=>{
+//             span.style.bottom = "60%";
+//     });
+//     showMenu.querySelector('span:nth-child(2)').style.transform = "rotate(135deg)";
+//     document.querySelector('.discover > span').textContent ="Hide All";
+//     }else{
+//         showMenu.querySelector('span').style.transform = "rotate(-45deg)";
+//         showMenu.querySelectorAll('span').forEach(span=>{
+//             span.style.bottom = "30%";
+//     });
+//         showMenu.querySelector('span:nth-child(2)').style.transform = "rotate(-135deg)";
+//     document.querySelector('.discover > span').textContent ="Show All";
+//     }
     
-})
+// })
