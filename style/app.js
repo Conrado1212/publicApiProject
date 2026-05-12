dea27e2b659fe309ccc97cad2c74712d496eff5b
Conrado1212 <conrado1212@gmail.com>
2026-05-12 18:45:57 +0200

    main

--- style/app.js
+++ style/app.js
@@ -134,3 +134,23 @@ async function loadMore(){
             loadMore();
         }
     });
+
+
+
+    //animate card
+
+    const cards = document.querySelectorAll('.game_card');
+    cards.forEach(card=>{
+        card.addEventListener('mouseenter',()=>{
+            //scale
+            card.style.transform = 'scale(1.1)';
+            //class
+            card.querySelector('.card_about').style.display="block";
+        });
+
+
+        card.addEventListener('mouseleave',()=>{
+            card.style.transform = 'scale(1)';
+            card.querySelector('.card_about').style.display="none";
+        });
+    });
