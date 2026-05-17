9234a83c992efe0caf8b612696173ec17cf95d40
Conrado1212 <conrado1212@gmail.com>
2026-05-17 13:13:26 +0200

    main

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const data = {
 app.get('/', async(req,res)=>{
   try {
     const result = await axios.get(API_URL + "games?key=" + API_KEY);
-    main();
+    main(1,20);
    // console.log(result.data.count);  
   //  console.log("DATA:", data);
     res.render("index.ejs",{ data: data, games: result.data.count});
